6b420b7fe7a4308e83fc5deacc21f118f3b44d58
Agah Shamekhi <Agah.Shamekhi@gmail.com>
2020-11-20 19:55:51 +0330

    ConsoleApp with DI

---
+++ ConsoleAppWithDIAndLogger/GreetingService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace ConsoleAppWithDIAndLogger
+{
+    public interface IGreetingService
+    {
+        void Execute();
+    }
+
+    public class GreetingService : IGreetingService
+    {
+        private readonly ILogger<GreetingService> logger;
+        private readonly IConfiguration configuration;
+
+        public GreetingService(ILogger<GreetingService> logger, IConfiguration configuration)
+        {
+            this.logger = logger;
+            this.configuration = configuration;
+        }
+
+        public void Execute()
+        {
+            var developer = configuration.GetValue<string>("Developer");
+
+            logger.LogInformation($"Hi {developer}. The time is: {DateTime.Now.ToShortTimeString()}");
+        }
+    }
+}
---
+++ ConsoleAppWithDIAndLogger/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+
+namespace ConsoleAppWithDIAndLogger
+{
+    class Program
+    {
+        private static IConfiguration Configuration;
+
+        static void Main()
+        {
+            #region Configuration
+            Configuration = new ConfigurationBuilder()
+                   .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                   .AddJsonFile("appsettings.serilog.json", optional: false, reloadOnChange: true)
+                   .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIROMENT") ?? "Production"}.json", optional: true, reloadOnChange: true)
+                   .Build();
+            #endregion
+
+            #region Serilog
+            Log.Logger = new LoggerConfiguration()
+                    .ReadFrom.Configuration(Configuration)
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console()
+                    .CreateLogger();
+            #endregion
+
+            #region Services
+            var host = Host.CreateDefaultBuilder()
+                    .ConfigureServices((context, services) =>
+                    {
+                        services.AddTransient<IGreetingService, GreetingService>();
+                    })
+                    .UseSerilog()
+                    .Build(); 
+            #endregion
+
+            var greetingSvc = ActivatorUtilities.CreateInstance<GreetingService>(host.Services);
+            greetingSvc.Execute();
+
+            Console.ReadLine();
+        }
+    }
+}
