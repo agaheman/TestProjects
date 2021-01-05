95f304183b7e9de77d23de29133e5469d592df46
Agah Shamekhi <Agah.Shamekhi@gmail.com>
2021-01-05 17:09:20 +0330

    ValueConverter

---
+++ MyValueConverter/SampleDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+
+namespace MyValueConverter
+{
+    public class SampleDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "SampleDb");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            var DateToIntConverter = new ValueConverter<DateTime, int>(
+                            v => int.Parse(v.Year.ToString("00") + v.Month.ToString("00") + v.Day.ToString("00")),
+                            v => new DateTime(GetDateParts(v).Year, GetDateParts(v).Month, GetDateParts(v).Day));
+
+            modelBuilder
+                .Entity<SampleModel>()
+                .Property(e => e.RegisterationDate)
+                .HasConversion(DateToIntConverter);
+        }
+
+        private static (int Year, int Month, int Day) GetDateParts(int date)
+        {
+            int year = int.Parse(date.ToString().Substring(0, 2));
+            int month = int.Parse(date.ToString().Substring(2, 2));
+            int day = int.Parse(date.ToString().Substring(4, 2));
+
+            return (year, month, day);
+        }
+    }
+}
---
+++ MyValueConverter/SampleModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MyValueConverter
+{
+    public class SampleModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime RegisterationDate { get; set; }
+    }
+}
