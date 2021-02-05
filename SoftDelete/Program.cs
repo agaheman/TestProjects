cf38e2c54747208ddf457487d155bf2701e5e816
Agah Shamekhi <Agah.Shamekhi@gmail.com>
2021-02-06 01:22:58 +0330

    DescriptionModal

--- SoftDelete/Program.cs
+++ SoftDelete/Program.cs
@@ -4,6 +4,7 @@ namespace SoftDelete
 {
     class Program
     {
+        public int MyProperty { get; set; }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
---
+++ WebApplication/Models/SampleModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.Models
+{
+    public class SampleModel
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+
+        [Display(Name = "نام")]
+        [Required(AllowEmptyStrings =false , ErrorMessage = "وارد کردن نام الزامی است.")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "نام خانوادگی")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "وارد کردن نام خانوادگی الزامی است.")]
+        public string LastName { get; set; }
+
+        [HiddenInput]
+        public string EditDescription { get; set; }
+    }
+}
